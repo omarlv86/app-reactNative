c7a27359edb80b8210de78fcec453b861c056061
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-04-19 13:33:31 -0500

    develop | Añadiendo Stacks de navegacion

---
+++ app/navigations/RestaurantsStack.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Restaurants  from "../screens/Restaurants";
+
+const Stack = createStackNavigator();
+
+export default function RestaurantsStack(){
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+              name="restaurants"
+              component={Restaurants}
+              options={{ title: "Restaurantes" }}
+            />
+            {/*}
+            <Stack.Screen
+              name="add-restaurant"
+              component={Restaurants}
+              options={{ title: "Añadir restaurante" }}
+            />
+            */}
+        </Stack.Navigator>
+    );
+}
