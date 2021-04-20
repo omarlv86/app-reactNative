08b041c10677123857c1a0ee173b1b2e4db950e5
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-04-20 11:55:29 -0500

    develop | Añadiendo el componente Loading a la vista de Account y mostrando el componente en el dispositivo

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     overlay: {
         height: 100,
         width: 200,
-        backgroundColor: "fff",
+        backgroundColor: "#fff",
         borderColor: "#00a680",
         borderWidth: 2,
         borderRadius: 10,
