9fdab25209a5ff3fba4ee31e091e6ee3dbab82ca
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-04-20 16:44:20 -0500

    develop | Creando nueva screen de login y se agrego al stack de Account

--- app/navigations/AccountStack.js
+++ app/navigations/AccountStack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Account from "../screens/Account/Account";
+import Login from "../screens/Account/Login";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,13 @@ export default function AccountStack() {
               component={Account}
               options={{ title: "Mi cuenta" }}
             />
+            <Stack.screen
+              name="Login"
+              component={Login}
+              options={{ title: "Iniciar sesión"}}
+            >
+            </Stack.screen>
         </Stack.Navigator>
+        
     )
 }
