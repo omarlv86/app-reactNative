4ab9fad7f324943f154422726cead8191e36370b
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-04-20 16:34:11 -0500

    develop | Modificando la vista de usuarios no loggeados

--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -1,10 +1,62 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
+import { Button } from "react-native-elements";
 
-export default function USerGuest(){
-    return(
-        <View>
-            <Text>UserGuest...</Text>
-        </View>
-    );
+export default function UserGuest() {
+    return (
+        <ScrollView centerContent={true} style={styles.viewBody}>
+            <Image 
+                source={require("../../../assets/img/user-guest.jpg")} resizeMode="contain"
+                style={styles.image}
+            />
+            <Text style={styles.title}> Consulta su perfil de 5 Tenedores</Text>
+            <Text style={styles.description}>
+                ¿Como describirias al mejor restaurante? Busca y visualiza los mejores restaurantes de forma sencilla, vota cual te ha gustado mas y comenta como ha sido experiencia.
+            </Text>
+            <View style={styles.viewBtn}>
+                <Button 
+                    title="Ver tu perfil"
+                    buttonStyle={styles.btnStyle}
+                    containerStyle={styles.btnContainer}
+                    onPress={() => console.log("Click!!!")}
+                />
+            </View>
+
+        </ScrollView>
+    )
 }
+
+const styles = StyleSheet.create({
+    viewBody:{
+        marginLeft: 30,
+        marginRight: 30,
+
+    },
+    image:{
+        height: 300,
+        width: "100%",
+        marginBottom: 40,
+    },
+    title: {
+        fontWeight:"bold",
+        fontSize: 19,
+        marginBottom:10,
+        textAlign: "center",
+    },
+    description: {
+        textAlign: "center",
+        marginBottom:20,
+    },
+    viewBtn: {
+        flex: 1,
+        alignItems: "center",
+    },
+    btnStyle: {
+        backgroundColor: "#00a680",
+        marginBottom:10
+
+    },
+    btnContainer: {
+        width:"70%",
+    }
+});
