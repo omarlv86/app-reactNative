f0eb87b0e14a054ca5bc6cddc5ceb305a39ca8f5
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-04-21 12:08:02 -0500

    develop | Guardando datos del formulario de registro en el estado

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -4,12 +4,26 @@ import { Input, Icon, Button } from "react-native-elements";
 
 export default function RegisterForm(){
     const [showPassword, setShowPassword] = useState(false);
-    const [showRepeatPassword, setShowRepeatPassword] = useState(false)
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+    const [formData, setFormData] = useState(defaultFormValue())
+
+    const onSubmit = () => {
+        console.log(formData);
+    }
+
+    const onChange = (e, type) => {
+        //console.log(type);
+        //console.log(e.nativeEvent.text);
+        //setFormData({ [type]: e.nativeEvent.text });
+        setFormData({ ...formData, [type]: e.nativeEvent.text })
+    }
+
     return(
         <View style={styles.formContainer}>
             <Input 
                 placeholder="Correo electronico"
                 containerStyle={styles.inputForm}
+                onChange={(e) => onChange(e, "email")}
                 rightIcon={
                     <Icon 
                         type="material-community"
@@ -23,6 +37,7 @@ export default function RegisterForm(){
                 containerStyle={styles.inputForm}
                 password={true}
                 secureTextEntry={showPassword ? false : true }
+                onChange={(e) => onChange(e, "password")}
                 rightIcon={
                     <Icon 
                         type="material-community"
@@ -37,6 +52,7 @@ export default function RegisterForm(){
                 containerStyle={styles.inputForm}
                 password={true}
                 secureTextEntry={showRepeatPassword ? false : true}
+                onChange={(e) => onChange(e, "repeatPassword")}
                 rightIcon={
                     <Icon 
                         type="material-community"
@@ -50,9 +66,18 @@ export default function RegisterForm(){
                 title="Registrar"
                 containerStyle={styles.btnContainerRegister}
                 buttonStyle={styles.btnRegister}
+                onPress={onSubmit}
             />
         </View>
     )
+}
+
+function defaultFormValue() {
+    return{
+        email: "",
+        password: "",
+        repeatPassword: ""
+    }
 }
 
 const styles = StyleSheet.create({
