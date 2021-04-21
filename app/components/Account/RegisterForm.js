d00137bba503b9f5e1d889b48bffc4cc91c231c1
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-04-21 10:50:52 -0500

    develop | Creando Vista para registrar, añadiendo estilos

---
+++ app/components/Account/RegisterForm.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Input, Icon, Button } from "react-native-elements";
+
+export default function RegisterForm(){
+    return(
+        <View style={styles.formContainer}>
+            <Input 
+                placeholder="Correo electronico"
+                containerStyle={styles.inputForm}
+            />
+            <Input 
+                placeholder="Contraseña"
+                containerStyle={styles.inputForm}
+                password={true}
+                secureTextEntry={true}
+            />
+            <Input 
+                placeholder="Repetir contraseña"
+                containerStyle={styles.inputForm}
+                password={true}
+                secureTextEntry={true}
+            />
+            <Button
+                title="Registrar"
+                containerStyle={styles.btnContainerRegister}
+                buttonStyle={styles.btnRegister}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    formContainer: {
+        //flex: 1,
+        //alignItems: "center",
+        //justifyContent: "center",
+        marginTop: 30,
+    },
+    inputForm: {
+        width: "100%",
+        marginTop: 20,
+    },
+    btnContainerRegister:{
+        marginTop: 20,
+        width: "95%",
+    },
+    btnRegister:{
+        backgroundColor: "#00a680",
+        
+    }
+});
