91fdb4f868bf0cf9d7a810b9cfc9cf6812793de8
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-04-30 13:48:04 -0500

    develop | Añadiendo opciones con list item en el componente AccountOptions

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -5,6 +5,7 @@ import Toast from "react-native-easy-toast";
 import * as firebase from "firebase";
 import Loading from "../../components/Loading";
 import InfoUser from "../../components/Account/InfoUser";
+import AccountOptions from "../../components/Account/AccountOptions";
 
 export default function UserLogged() {
     const [userInfo, setUserInfo] = useState(null)
@@ -32,7 +33,7 @@ export default function UserLogged() {
                             setLoadingText={setLoadingText}
                             />}
 
-            <Text>AccountOptions</Text>
+            <AccountOptions userInfo={userInfo} toastRef={toastRef} />
 
             <Button 
               title="Cerrar sesión" 
