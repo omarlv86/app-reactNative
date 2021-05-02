9658d820e7e9878587a8e0c38cc51fdf02e418a4
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-05-02 17:38:20 -0500

    develop | Cambiando contraseña, añadiendo estilos al formulario, creando function para validar los datos del formulario y realizar la peticion al api de firebase

--- app/components/Account/AccountOptions.js
+++ app/components/Account/AccountOptions.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View } from "react-native";
 import { ListItem } from "react-native-elements";
 import { map } from "lodash";
 import Modal from "../Modal";
 import ChangeDisplayNameForm from "./ChangeDisplayNameForm";
 import ChangeEmailForm from "./ChangeEmailForm";
+import ChangePasswordForm from "./ChangePasswordForm";
 
 export default function AccountOptions(props){
     const {userInfo, toastRef, setReloadUserInfo} = props;
@@ -32,11 +33,13 @@ const selectedComponent = (key) => {
                 toastRef={toastRef}
                 setReloadUserInfo={setReloadUserInfo}
               />
-            );
+            )
             setShowModal(true);
             break
         case "password":
-            setRenderComponent(<Text>Cambiando contraseña</Text>);
+            setRenderComponent(
+                <ChangePasswordForm setShowModal={setShowModal} toastRef={toastRef} />
+            )
             setShowModal(true);
             break
         default:
