cda869ebc7195198795331af46dcc6f17b2f917f
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-05-03 11:13:23 -0500

    Develop | Añadiendo boton para añadir nuevos restaurantes y mostrandolo solo cuando el usuario este loggeado

---
+++ app/screens/Restaurants/Restaurants.js
@@ -0,0 +1,48 @@
+import React,{ useState, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Icon } from "react-native-elements";
+import { firebaseApp } from "../../Utils/firebase";
+import firebase from "firebase/app";
+
+export default function Restautants(){
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((userInfo) => {
+          console.log(userInfo);
+          setUser(userInfo);
+        })
+    }, [])
+
+    return(
+        <View style={styles.viewBody}>
+            <Text>Restaurants...</Text>
+
+            { user && (
+                <Icon
+                reverse 
+                type="material-community" 
+                name="plus" 
+                color="#00a680"
+                containerStyle={styles.btnContainer}
+              />
+            )}
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    btnContainer:{
+        position:"absolute",
+        bottom: 0,
+        right: 10,
+        shadowColor: "black",
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.5,
+    }
+});
