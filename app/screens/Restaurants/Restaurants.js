ec54e5aac7717ba8808285f45f9b532f3108cece
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-05-06 23:38:14 -0500

    develop | Obteniendo la lista de los ultimos 5 restaurantes agregados

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -3,15 +3,50 @@ import { StyleSheet, View, Text } from "react-native";
 import { Icon } from "react-native-elements";
 import { firebaseApp } from "../../Utils/firebase";
 import firebase from "firebase/app";
+import "firebase/firestore";
+
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Restautants(props){
     const { navigation } = props;
     const [user, setUser] = useState(null);
+    const [restaurants, setRestaurants] = useState([]);
+    const [totalRestaurants, setTotalRestaurants] = useState(0);
+    const [startRestaurant, setStartRestaurant] = useState(null);
+    const limitRestaurant = 5;
+    //console.log(totalRestaurants);
+    console.log(restaurants);
 
     useEffect(() => {
         firebase.auth().onAuthStateChanged((userInfo) => {
           //console.log(userInfo);
           setUser(userInfo);
+        })
+    }, []);
+
+    useEffect(() => {
+        db.collection("restaurants").get().then((snap) => {
+            setTotalRestaurants(snap.size);
+        });
+
+        const resultRestaurants = [];
+
+        db.collection("restaurants")
+        .orderBy("createAt", "desc")
+        .limit(limitRestaurant).get().then((response) => {
+            setStartRestaurant(response.docs[response.docs.length -1]);
+
+            response.forEach((doc) => {
+                //console.log(doc.data());
+                //console.log(doc.id)
+                const restaurant = doc.data();
+                restaurant.id = doc.id;
+                //console.log(restaurant);
+
+                resultRestaurants.push(restaurant);
+            });
+            setRestaurants(resultRestaurants);
         })
     }, [])
 
