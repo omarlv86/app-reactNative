df70c712f15afa83a9dcee555eb55ce5db55267b
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-05-13 15:17:45 -0500

    develop | Obteniendo la informacion del restaurante y actualizando estado

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,8 +1,30 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { firebaseApp } from "../../Utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Restaurant(props) {
-    console.log(props);
+    const { navigation, route } = props;
+    const { id, name } = route.params;
+    const [restaurant, setRestaurant] = useState(null);
+    console.log(restaurant);
+
+    //navigation.setOptions({ title: name});
+
+    useEffect(() => {
+        db.collection("restaurants")
+          .doc(id)
+          .get()
+          .then((response) => {
+              const data = response.data();
+              data.id = response.id;
+              setRestaurant(data);
+          })
+    }, [])
+
     return (
         <View>
             <Text>Restaurante info...</Text>
