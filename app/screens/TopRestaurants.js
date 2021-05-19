ffe07d796a2c1561a1e8b08adf30d1505a9e4c00
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-05-19 18:02:04 -0500

    develop | Seccion de raking

--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect, useRef} from "react";
+import { View } from "react-native";
+import Toast from "react-native-easy-toast";
+import ListTopRestaurants from "../components/Ranking/ListTopRestaurants";
 
-export default function TopRestaurants() {
+import { firebaseApp } from "../Utils/firebase"
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
+
+export default function TopRestaurants(props) {
+    //console.log(props);
+    const { navigation } = props;
+    const [restaurants, setRestaurants] = useState([]);
+    const toastRef = useRef();
+
+    console.log(restaurants);
+
+    useEffect(() => {
+        db.collection("restaurants")
+        .orderBy("rating", "desc")
+        .limit(5)
+        .get()
+        .then((response) => {
+            const restaurantArray = [];
+            response.forEach((doc) => {
+                //console.log(doc.data());
+                const data = doc.data();
+                data.id = doc.id;
+                restaurantArray.push(data);
+            });
+            setRestaurants(restaurantArray);
+        })
+    }, [])
+
     return(
         <View>
-            <Text>Top Restaurants...</Text>
+            <ListTopRestaurants restaurants={restaurants} navigation={navigation}/>
+            <Toast ref={toastRef} positition="center" opacity={0.9} />
         </View>
     );
 }
