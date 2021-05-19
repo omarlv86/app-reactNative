0929d9b3ee23d4b59f12b03ac17282f36b283652
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-05-19 18:55:37 -0500

    develop | Buscador de restaurantes terminado

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,10 +1,78 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, FlatList, Image } from "react-native";
+import {SearchBar, ListItem, Icon } from "react-native-elements";
+import { FireSQL } from "firesql";
+import firebase from "firebase/app";
 
-export default function Favorites() {
+const fireSQL = new FireSQL(firebase.firestore(), {includeId: "id"});
+
+export default function Favorites(props) {
+    const { navigation } = props;
+    const [search, setSearch] = useState("");
+    const [restaurants, setRestaurants] = useState([]);
+
+    //console.log(restaurants);
+    useEffect(() => {
+        if(search){
+          fireSQL.query(`SELECT * FROM restaurants WHERE name LIKE '${search}%'`)
+          .then((response) => {
+            //console.log(response);
+            setRestaurants(response);
+          })
+        }
+      
+    }, [search])
     return(
         <View>
-            <Text>Favorites...</Text>
+            <SearchBar
+              placeholder="Busca tu restaurante..."
+              onChangeText={(e) => setSearch(e)}
+              value={search}
+              containerStyle={StyleSheet.searchBar}
+            />
+            {restaurants.length == 0 ? (
+              <NotFoundRestaurants />
+            ):(
+                <FlatList
+                  data={restaurants}
+                  renderItem={(restaurant) => 
+                    <Restaurant restaurant={restaurant}  navigation={navigation}/>}
+                  keyExtractor={(item, index) => index.toString()}
+                />
+            )}
         </View>
     );
 }
+
+function NotFoundRestaurants(){
+    return(
+        <View style={{ flex:1, alignItems: "center"}}>
+          <Image 
+            source={require("../../assets/img/no-result-found.png")}
+            resizeMode="cover"
+            style={{width: 200, height: 200}}
+          />
+        </View>
+    )
+}
+
+function Restaurant(props){
+    const { restaurant, navigation } = props;
+    const { id, name, images} = restaurant.item
+    return(
+        <ListItem
+          title={name}
+          leftAvatar={{
+              source:images[0] ? { uri: images[0]} : require("../../assets/img/no-image.png")
+            }}
+          rightIcon={<Icon type="material-community" name="chevron-right"/>}
+          onPress={() => navigation.navigate("restaurants", {screen:"restaurant", params: { id, name }})}
+        />
+    )
+}
+const styles = StyleSheet.create({
+    searchBar: {
+        marginBottom: 20,
+        
+    }
+})
